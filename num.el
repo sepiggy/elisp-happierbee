3a587d8e0420255a98acec0a75340220551a1d4c
sepiggy <jms1209@qq.com>
2017-10-01 14:56:34 +0800

    3 completed

---
+++ num.el
@@ -0,0 +1,87 @@
+#b101100
+44
+
+#o54
+44
+
+#x2c
+44
+
+#24r1k
+44
+
+(/ 0.0 0.0)
+-0.0e+NaN
+
+(integerp 1.)
+t
+
+(integerp 1.0)
+nil
+
+(floatp 1.)
+nil
+
+(floatp -0.0e+NaN)
+t
+
+(numberp 1)
+t
+
+(zerop 1)
+nil
+
+(wholenump -1)
+nil
+
+(setq foo (- (+ 1.0 1.0e-3) 1.0))
+0.0009999999999998899
+
+(setq bar 1.0e-3)
+0.001
+
+(= foo bar)
+nil
+
+(= 1.0 1)
+t
+
+(eql 1.0 1)
+nil
+
+(truncate 1.2)
+1
+
+(floor 1.7)
+1
+
+(ceiling -1.2)
+-1
+
+(round 1.2)
+1
+
+(/ 5 6.0)
+0.8333333333333334
+
+(setq foo 10)
+10
+
+(setq foo (1+ foo))
+11
+
+(setq foo (1- foo))
+10
+
+
+
+
+
+#o54
+#x2c
+
+(/ 0.0 0.0)
+
+(integerp 1.)
+(integerp 1.0)
+(floatp 1.)t
